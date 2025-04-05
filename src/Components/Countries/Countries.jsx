c30637a2125de679c7a54436e3ee4cd4f6466cee
shohag390 <shohag390@users.noreply.github.com>
2025-04-05 18:26:03 +0600

    All Project Ok

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,14 +1,30 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
+import Country from '../Country/Country';
 
 const Countries = ({ countriesPromise }) => {
 
     const countries = use(countriesPromise);
-    console.log(countries);
+
+    const [visitedItem, setVisitedItem] = useState([]);
+
+    const handleVisitedItem = (cuntry) => {
+        setVisitedItem([...visitedItem, cuntry]);
+    }
 
 
     return (
-        <div>
-
+        <div className='px-[50px]'>
+            <div className='flex items-center justify-between'>
+                <h4 className='text-[green] text-[25px] font-medium uppercase pt-[20px] pb-[10px]'>Countries List</h4>
+                <h4 className='text-[green] text-[25px] font-medium uppercase pt-[20px] pb-[10px]'>Visited List: {visitedItem.length}</h4>
+            </div>
+            <div className='grid grid-cols-5 gap-[10px]'>
+                {countries?.map((cuntry) => <Country
+                    key={cuntry?.name?.common}
+                    cuntry={cuntry}
+                    handleVisitedItem={handleVisitedItem} />)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Country = () => {
+const Country = ({ cuntry, handleVisitedItem }) => {
+
+
+    const [visited, setVisited] = useState(false);
+
+    const handleToggle = () => {
+        setVisited(!visited)
+        handleVisitedItem(cuntry);
+    }
+
+
     return (
-        <div>
-
+        <div className={`p-[10px] border-[1px] border-[green] ${visited ? "bg-[green]" : "bg-transparent"}`}>
+            <img className='h-[150px] w-full' src={cuntry?.flags?.png} alt="image" />
+            <h4 className='text-[#fff] font-medium text-[18px] line-clamp-1 pt-[5px]'>{cuntry?.name?.common}</h4>
+            <button onClick={handleToggle} className={`w-full py-[8px] bg-[green] text-[#fff] mt-[10px] ${visited ? "bg-[#fff] text-[green]" : "bg-[green]"}`}>{visited ? "Visited" : "Not Visited"}</button>
         </div>
     );
 };
