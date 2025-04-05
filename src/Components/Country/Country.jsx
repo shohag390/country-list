79f7e6d179ecb917bac82859bf9755eb35632a79
shohag390 <shohag390@users.noreply.github.com>
2025-04-05 18:31:32 +0600

    All Ok

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -15,7 +15,7 @@ const Country = ({ cuntry, handleVisitedItem }) => {
         <div className={`p-[10px] border-[1px] border-[green] ${visited ? "bg-[green]" : "bg-transparent"}`}>
             <img className='h-[150px] w-full' src={cuntry?.flags?.png} alt="image" />
             <h4 className='text-[#fff] font-medium text-[18px] line-clamp-1 pt-[5px]'>{cuntry?.name?.common}</h4>
-            <button onClick={handleToggle} className={`w-full py-[8px] bg-[green] text-[#fff] mt-[10px] ${visited ? "bg-[#fff] text-[green]" : "bg-[green]"}`}>{visited ? "Visited" : "Not Visited"}</button>
+            <button onClick={handleToggle} className={`w-full py-[8px] mt-[10px] ${visited ? "bg-[#fff] text-[green]" : "bg-[green] text-[#fff]"}`}>{visited ? "Visited" : "Not Visited"}</button>
         </div>
     );
 };
