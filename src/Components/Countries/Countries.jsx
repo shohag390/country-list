3f46191b1f8df17bdae7d5a9b34a7a4815f62590
shohag390 <shohag390@users.noreply.github.com>
2025-04-06 17:23:17 +0600

    Ul Item Add

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -6,6 +6,8 @@ const Countries = ({ countriesPromise }) => {
     const countries = use(countriesPromise);
 
     const [visitedItem, setVisitedItem] = useState([]);
+    console.log(visitedItem);
+
 
     const handleVisitedItem = (cuntry) => {
         setVisitedItem([...visitedItem, cuntry]);
@@ -18,6 +20,9 @@ const Countries = ({ countriesPromise }) => {
                 <h4 className='text-[green] text-[25px] font-medium uppercase pt-[20px] pb-[10px]'>Countries List</h4>
                 <h4 className='text-[green] text-[25px] font-medium uppercase pt-[20px] pb-[10px]'>Visited List: {visitedItem.length}</h4>
             </div>
+            <ul className='text-[#fff] grid grid-cols-5 gap-[10px] pb-[15px]'>
+                {visitedItem?.map((country) => <li className='border-[1px] border-[#fff] p-[10px] line-clamp-1' key={country?.name?.common}>{country?.name?.common}</li>)}
+            </ul>
             <div className='grid grid-cols-5 gap-[10px]'>
                 {countries?.map((cuntry) => <Country
                     key={cuntry?.name?.common}
