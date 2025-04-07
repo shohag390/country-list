6e59b804d3c6f2efc8bf79b3242d50f749ccddd8
shohag390 <shohag390@users.noreply.github.com>
2025-04-07 13:29:23 +0600

    All Project Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import Countries from './Components/Countries/Countries';
+import Navbar from './Components/Navbar/Navbar';
 
 
 const countriesPromise = fetch('https://restcountries.com/v3.1/all')
@@ -8,9 +9,11 @@ const countriesPromise = fetch('https://restcountries.com/v3.1/all')
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Suspense fallback={<h1 className='text-[#fff] text-center '>Loading.....</h1>}>
         <Countries countriesPromise={countriesPromise} />
       </Suspense>
+
     </div >
   );
 };
